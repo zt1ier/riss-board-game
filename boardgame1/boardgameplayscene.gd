efbc0032b9f4181427b5853c2ef1e0b214b91d54
z-tier <fglemeric@gmail.com>
2025-08-08 12:07:25 +0800

    idk

--- boardgame1/boardgameplayscene.gd
+++ boardgame1/boardgameplayscene.gd
@@ -2,14 +2,19 @@ extends Node2D
 
 
 @onready var tilemap = $TileMap
-@onready var player_piece = $player1spriteGreen
+@onready var player_piece: Sprite2D = $player1spriteGreen
 @onready var rollresult = $rollresult
-var current_tile = 1
+var current_tile: int = 1
+
+
+func _ready() -> void:
+	move_piece_to_tile(current_tile)
+
 
 func get_tile_position(tile_num: int) -> Vector2i:
 	var index := tile_num - 1
 	@warning_ignore("integer_division")
-	var row := index // 10
+	var row := index / 10
 	var col := index % 10
 	if row % 2 == 1:
 		col = 9 - col
@@ -22,16 +27,16 @@ func move_piece_to_tile(tile_num: int):
 	var center_tile: Vector2i = tile_offset + Vector2i(3, 3)     # center of 6x6
 
 	var cell_pos: Vector2 = tilemap.map_to_local(center_tile)   # convert to local
-	var global_pos: Vector2 = tilemap.to_global(cell_pos)       # convert to global
+	#var global_pos: Vector2 = tilemap.to_global(cell_pos)       # convert to global
 
-	player_piece.global_position = global_pos                   # move piece
+	player_piece.global_position = cell_pos       # move piece
 
 	print("Moving to tile", tile_num, " -> tile grid:", center_tile, " -> pos:", player_piece.global_position)
 
 
 
 func _on_dicerollbutton_pressed():
-	var dice_roll = randi() % 6 + 1
+	var dice_roll = randi_range(1, 6) # use randi_range(1, 6) for a six-sided die instead of randi() % 6 + 1
 	$rollresult.text = "u got" + str(dice_roll)
 	var target_tile = current_tile + dice_roll
 	if target_tile > 100:
@@ -40,6 +45,7 @@ func _on_dicerollbutton_pressed():
 	# Move one tile at a time with delay
 	for i in range(current_tile + 1, target_tile + 1):
 		move_piece_to_tile(i)
-		await get_tree().create_timer(0.3).timeout
+		await get_tree().create_timer(0.5).timeout
 
 	current_tile = target_tile
+	print(player_piece.global_position)
